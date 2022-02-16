121764092a6cb2bbd8a7061b6e641dd3a2454fbb
David Schwerd <dmsch656@gmail.com>
2022-02-16 13:53:41 -0700

    buttons for prior searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,47 @@
 var formEl =document.getElementById('city-form');
 var cityInputEl = document.getElementById('city-input');
-var cities = JSON.parse(localStorage.getItem('cities')) || []
+var cities = getCitiesFromLS();
+var priorSearchEl = document.getElementById('prior-search');
 
-function getForecast(city){
-
+function getForecast(city) {
+    console.log('Getting forecast in ' + city);
 }
 
-function getCitiesFromLS(){
+function getCitiesFromLS() {
     return JSON.parse(localStorage.getItem('cities')) || []
 }
 
-function setCitiesToLS(){
-    localStorage.setItem(JSON.stringify(cities));
+function setCitiesToLS() {
+    localStorage.setItem('cities', JSON.stringify(cities));
 }
 
-formEl.addEventListener('submit', function(event){
+function saveCityToLS(city) {
+
+}
+
+function createPriorSearch() {
+    priorSearchEl.innerHTML = "";
+    cities = getCitiesFromLS();
+    for(var i = 0; i < cities.length; i++) {
+        var cityButton = document.createElement('button');
+        cityButton.textContent = cities[i];
+        cityButton.setAttribute('data-city', cities[i])
+        cityButton.addEventListener('click', function(event){
+            var city = event.target.getAttribute('data-city')
+            getForecast(city);
+        })
+        priorSearchEl.appendChild(cityButton);
+    }
+}
+
+formEl.addEventListener('submit', function(event) {
     event.preventDefault();
     var value = cityInputEl.value;
     getForecast(cityInputEl.value);
+
     cities.push(value);
+    setCitiesToLS();
+    cityInputEl.value = "";
 })
 
+createPriorSearch();
